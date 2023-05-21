f3b3e9772ee1b0dbbd8f45120b1410f28e38ec34
Michcic7 <gaska.michal7@gmail.com>
2023-05-21 19:50:58 +0200

    Readonly class fields

--- ScraperApp/Serialization/SerializationHandler.cs
+++ ScraperApp/Serialization/SerializationHandler.cs
@@ -7,8 +7,8 @@ internal class SerializationHandler
 {
     private readonly Serializer _serializer = new();
 
-    private int _startYearDrivers = 1950;
-    private int _startYearTeams = 1958;
+    private readonly int _startYearDrivers = 1950;
+    private readonly int _startYearTeams = 1958;
 
     internal void HandleDrivers()
     {
